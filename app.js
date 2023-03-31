f7281b3c627a77b8fd75a35f7b0039ad97618db8
Ion Rata <iontherata@gmail.com>
2023-03-31 12:50:08 +0300

    boilerplate init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log('HELLO WORLD');
+})
